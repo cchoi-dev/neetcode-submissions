9699c45a2036cfd145d9a76b75f63a040d0f63d6
cchoi-dev <cchoi7914@gmail.com>
2026-06-09 02:34:23 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // To determine whether an input is a palindrome, one way we could go about this is
+        // to have two pointers that continue until they're equal or 'past' each other.
+        for (auto& c : s) {
+            c = std::tolower(static_cast<unsigned char>(c));
+        }
+        auto start = s.begin();
+        auto end = s.end();
+        end--;
+        if (!std::isalnum(*start)) {
+            iterateUntilAlphanumeric(start, true);
+        }
+        if (!std::isalnum(*end)) {
+            iterateUntilAlphanumeric(end, false);
+        }
+        while (start <= end) {
+            // Compare values and continue if the characters are the same
+            // printf("%c - %c", *start, *end);
+            if (*start != *end) {
+                return false;
+            }
+            iterateUntilAlphanumeric(start, true);
+            iterateUntilAlphanumeric(end, false);
+        }
+
+        return true;
+    }
+
+    void iterateUntilAlphanumeric(string::iterator& it, bool iterateForward) {
+        // printf("Started at %c, iterating ", *it);
+        // printf("%s.\n", iterateForward ? "forwards" : "backwards");
+        do {
+            if (iterateForward) {
+                it++;
+            }
+            else {
+                it--;
+            }
+        } while (!std::isalnum(*it));
+        // printf("Ended at %c\n", *it);
+    }
+};
