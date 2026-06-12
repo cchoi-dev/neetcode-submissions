d818bc04362087f65f4bbe03058e8defeb1bbcec
cchoi-dev <cchoi7914@gmail.com>
2026-06-12 22:37:00 +0900

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> retVec;
+        for (int i = 0; i <= n; i++) {
+            retVec.push_back(countBitsForInt(i));
+        }
+
+        return retVec;
+    }
+
+    int countBitsForInt(int n) {
+        int sum = 0;
+        for (int i = 0; i < 32; i++) {
+            sum += (n & 0x01);
+            n = n >> 1;
+        }
+        printf("Sum: %d\n", sum);
+        return sum;
+    }
+};
