7aacf504f7b238eda3fc751de261aff648f64ba6
cchoi-dev <cchoi7914@gmail.com>
2026-06-17 19:06:42 +0900

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        // The number of ways we can reach a step n is the addition
+        // of the number of ways we can reach step n-1 + n-2,
+        // as the only way we can reach that step is by 1 or 2 steps.
+        // We can use this insight to develop our algorithm (is this a DP problem?).
+
+        // int stepsToIdx[n];
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else if (n == 2) {
+            return 2;
+        }
+        vector<int> stepsToIdx(n+1);
+        stepsToIdx[0] = 0;
+        stepsToIdx[1] = 1;
+        stepsToIdx[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            stepsToIdx[i] = stepsToIdx[i-1] + stepsToIdx[i-2];
+        }
+
+        return stepsToIdx[n];
+    }
+};
